5597d3d2ef88d216da0a4547588f6fd0b7428cf0
Marisa You <marisa.you@gmail.com>
2020-09-01 20:52:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,14 @@
+def reverse(word)
+  reversed_word = ""
+  for i in 0...word.length 
+    reversed_word += word[word.length-i-1]
+  end
+end
+
 def reverse_each_word(str)
   str_array = str.split(" ")
   str_array.each do |element|
-      
+      reverse(element)
   end
   
   return str_array.join(" ")
